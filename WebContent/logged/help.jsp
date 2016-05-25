cb1cdc5845891b0a5e36b5368481887377cd1cb2
FlyzZx <nicolasfaraci@outlook.com>
2016-05-25 19:03:16 +0200

    Placing help module
    -Added Help servlet
    -Added help.jsp

---
+++ WebContent/logged/help.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+    <%@ taglib prefix="f" uri="http://java.sun.com/jsp/jstl/fmt" %>
+    <!-- La lib FMT permet le formatage des dates sans utiliser de code JAVA -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<link rel="stylesheet" type="text/css" href="css/layout.css" />
+<script type="text/javascript" src="js/jquery-1.12.1.min.js" ></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+<title>Profil Personnel</title>
+</head>
+<body>
+<c:import url="navigation.jsp"></c:import>
+<c:import url="headerb.jsp"></c:import>
+<div class="content2">
+	<h3>Aide à l'utilisation de l'application</h3>
+</div>
+</body>
+</html>
---
+++ src/projet/suiviapprenti/servlets/Help.java
@@ -0,0 +1,33 @@
+package projet.suiviapprenti.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Help
+ */
+@WebServlet("/help")
+public class Help extends HttpServlet {
+	private static final long serialVersionUID 	= 1L;
+	public static final String VUE_HELP			= "/logged/help.jsp";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Help() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher(VUE_HELP).forward(request, response);
+	}
+
+}
